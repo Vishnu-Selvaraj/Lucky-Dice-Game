ccde19fa849b59bb3830e7ecd43fbdac5f64903d
VISHNU S RAJ <vishnusraj1997@gmail.com>
2025-12-05 14:22:17 +0530

    implemented dice roll sound

--- src/components/gamePageComp/rollDices/RollDices.tsx
+++ src/components/gamePageComp/rollDices/RollDices.tsx
@@ -1,10 +1,12 @@
 import { useValue } from "../../../context/ValueContext";
+import { useRef } from "react";
 import dice_1 from "../../../assets/images/dice_1.png";
 import dice_2 from "../../../assets/images/dice_2.png";
 import dice_3 from "../../../assets/images/dice_3.png";
 import dice_4 from "../../../assets/images/dice_4.png";
 import dice_5 from "../../../assets/images/dice_5.png";
 import dice_6 from "../../../assets/images/dice_6.png";
+import rollDiceAudio from '../../../assets/audio/dice-roll.mp3'
 
 interface Props {
   showRule: boolean;
@@ -15,6 +17,8 @@ const RollDices = ({ setRuleState, showRule }: Props) => {
   const { generateRandomDiceNumber, selectedDiceNumber, setResultScore } =
     useValue();
 
+    const audioRef = useRef<HTMLAudioElement | null>(null);
+
   const diceImages: Record<number, string> = {
     1: dice_1,
     2: dice_2,
@@ -24,13 +28,26 @@ const RollDices = ({ setRuleState, showRule }: Props) => {
     6: dice_6,
   };
 
+  const handleDiceClick = ()=>{
+    if (audioRef.current) {
+      audioRef.current.currentTime = 0; // restart sound each click
+      audioRef.current.play();
+    }
+    setTimeout(()=>{
+      generateRandomDiceNumber()
+    },200)
+  }
+
   return (
     <section className="pt-10 flex flex-col items-center">
+      {/* AUDIO TAG */}
+      <audio ref={audioRef} src={rollDiceAudio} preload="auto" />
+  
       <img
         src={`${diceImages[selectedDiceNumber]}`}
         alt={`dice_${selectedDiceNumber}`}
         className="w-50 h-50 cursor-pointer"
-        onClick={generateRandomDiceNumber}
+        onClick={handleDiceClick}
       />
       <h6 className="font-medium text-xl py-3">Click On Dice to Roll</h6>
       <button
